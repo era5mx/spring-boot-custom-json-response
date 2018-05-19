415054e7ade96d8d659a978da8627b767e41e234
David Rengifo <alexander@extend.com.mx>
2018-05-19 05:21:27 -0500

    tre

--- src/main/java/com/anglobal/spring/jsonresponse/service/impl/JSONResponseCustomServiceImpl.java
+++ src/main/java/com/anglobal/spring/jsonresponse/service/impl/JSONResponseCustomServiceImpl.java
@@ -4,10 +4,16 @@
  */
 package com.anglobal.spring.jsonresponse.service.impl;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
 
 import com.anglobal.spring.jsonresponse.dto.CustomDTO;
 import com.anglobal.spring.jsonresponse.response.IResponseCustomDTO;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 /**
@@ -23,9 +29,27 @@ public class JSONResponseCustomServiceImpl implements com.anglobal.spring.jsonre
 	 */
 	@Override
 	public IResponseCustomDTO getCustomResponse() {
-
-		CustomDTO dto = new CustomDTO();
-
+		String path = "C:\\Users\\alexander\\git\\spring-boot-custom-json-response\\src\\main\\java\\com\\anglobal\\spring\\jsonresponse\\dto\\json\\response-gatewayap-mxmultivendoratm.json";
+		CustomDTO dto = loadJSON(path);
+		return dto;
+	}
+	
+	
+	private CustomDTO loadJSON(String path) {
+		ObjectMapper mapper = new ObjectMapper();
+		CustomDTO dto = null;
+		try {
+			dto = mapper.readValue(new File(path), CustomDTO.class);
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return dto;
 	}
 
